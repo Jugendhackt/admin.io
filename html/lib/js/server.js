0d2dda91f5bee9b4cf6708e787abee36d184b491
bixilon <bixilon@bixilon.de>
2020-03-07 14:37:48 +0100

    Ping Implementation

--- html/lib/js/server.js
+++ html/lib/js/server.js
@@ -10,27 +10,50 @@ class Server {
         return this.name;
     }
 
+    getPing() {
+        switch (this.infected) {
+            case 0:
+            case 1:
+            case 2: {
+                //normal
+                return Math.round(Math.random() * 10 + 1) + "ms";
+            }
+            case 3: {
+                //minimal
+                return Math.round(Math.random() * 50 + 10) + "ms";
+            }
+            case 4: {
+                //attack is running, minimal higher
+                return Math.round(Math.random() * 100 + 50) + "ms";
+            }
+            case 5: {
+                //big attack, responses slow
+                return Math.round(Math.random() * 1000 + 300) + "ms";
+            }
+        }
+    }
+
     getUsage() {
         if (this.infected > 0) {
             switch (this.infected) {
                 case 1: {
-                    //low
+                    //idle
                     return Math.round(Math.random() * 20 + 20) + "%";
                 }
                 case 2: {
-                    //low
+                    //normal
                     return Math.round(Math.random() * 20 + 30) + "%";
                 }
                 case 3: {
-                    //low
+                    //minimal attack
                     return Math.round(Math.random() * 20 + 50) + "%";
                 }
                 case 4: {
-                    //low
+                    //attack / heavy load
                     return Math.round(Math.random() * 20 + 80) + "%";
                 }
                 case 5: {
-                    //low
+                    //attack / max load
                     return "100%";
                 }
             }
@@ -62,17 +85,19 @@ function addServer() {
     cell.innerText = servers[servers.length - 1].getUsage();
     cell.classList.add("server-usage");
     cell = row.insertCell(3);
-    cell.innerText = "123ms";
+    cell.innerText = servers[servers.length - 1].getPing();
     cell.classList.add("server-ping");
     cell = row.insertCell(4);
-    cell.innerHTML = '<td class="server-actions"><button class="server-button btn-danger btn server-actions-shutdown" onclick="this.disabled = true;">Herunterfahren</button><button class="server-button btn-success btn server-actions-start">Starten</button><button class="server-button btn-primary btn server-actions-start">Terminal</button></td>';
+    cell.innerHTML = '<td class="server-actions"><button class="server-button btn-danger btn server-actions-shutdown" onclick="this.disabled = true;">Herunterfahren</button><button disabled class="server-button btn-success disabled btn server-actions-start">Starten</button><button class="server-button btn-primary btn server-actions-start">Terminal</button></td>';
     cell.classList.add("server-actions");
 
 }
 
+//refresh values
 window.setInterval(function () {
     $(".single-server").each(function (index) {
         $(this).children(".server-usage").text(servers[index].getUsage());
+        $(this).children(".server-ping").text(servers[index].getPing());
     });
 }, 1000);
 
